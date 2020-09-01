5cf6b0bf96b7ad9e94b53740006f1bc9a2ecca0f
zjr1234 <64582965+zjr1234@users.noreply.github.com>
2020-09-01 16:46:26 +0800

    Add files via upload

---
+++ listPythonTest.py
@@ -0,0 +1,23 @@
+import argparse
+import sys
+from pathlib import Path
+
+
+def main(args):
+    parser = argparse.ArgumentParser(description='Returns sum/minus of two arguments')
+    parser.add_argument("--list_arg", type=list, required=True)
+    parser.add_argument("--sum", type=str, required=True)
+    parser.add_argument("--minus", type=str, required=True)
+    args = parser.parse_args(args)
+
+    Path(args.sum).parent.mkdir(parents=True, exist_ok=True)
+    with open(args.sum, 'w') as sum_path:
+        sum_path.write('{}'.format(args.list_arg[0] + args.list_arg[1]))
+        
+    Path(args.minus).parent.mkdir(parents=True, exist_ok=True)
+    with open(args.minus, 'w') as minus_path:
+        minus_path.write('{}'.format(args.list_arg[0] - args.list_arg[1]))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
