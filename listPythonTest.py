7909c4de231e5cb4f51fdb7ddcf12f946112b958
zjr1234 <64582965+zjr1234@users.noreply.github.com>
2020-09-02 14:11:13 +0800

    Update listPythonTest.py

--- listPythonTest.py
+++ listPythonTest.py
@@ -4,19 +4,21 @@ from pathlib import Path
 
 
 def main(args):
+    arg1 = args[0][0]["arg1"]
+    arg2 = args[0][0]["arg2"]
     parser = argparse.ArgumentParser(description='Returns sum/minus of two arguments')
-    parser.add_argument("--list_arg", type=list, required=True)
+    parser.add_argument("--arg1", type=int, required=True)
+    parser.add_argument("--arg2", type=int, required=True)
     parser.add_argument("--sum", type=str, required=True)
     parser.add_argument("--minus", type=str, required=True)
     args = parser.parse_args(args)
-    print(args.list_arg[0][0]["arg1"])
     Path(args.sum).parent.mkdir(parents=True, exist_ok=True)
     with open(args.sum, 'w') as sum_path:
-        sum_path.write('{}'.format(args.list_arg[0][0]["arg1"] + args.list_arg[0][0]["arg2"]))
+        sum_path.write('{}'.format(args.arg1 + args.arg2))
         
     Path(args.minus).parent.mkdir(parents=True, exist_ok=True)
     with open(args.minus, 'w') as minus_path:
-        minus_path.write('{}'.format(args.list_arg[0][0]["arg1"] - args.list_arg[0][0]["arg2"]))
+        minus_path.write('{}'.format(args.arg1 - args.arg2))
 
 
 if __name__ == '__main__':
