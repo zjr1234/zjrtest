65b65845439fa1090874e7658a1818178f955df1
zjr1234 <64582965+zjr1234@users.noreply.github.com>
2021-11-03 14:16:40 +0800

    Update listPy.py

--- listPy.py
+++ listPy.py
@@ -6,7 +6,7 @@ import time
 
 def main(args):
     parser = argparse.ArgumentParser(description='Returns string arguments')
-    parser.add_argument("--output1", type=list, required=True) #出参output1，自定义参数类型：string
+    parser.add_argument("--output1", type=object, required=True) #出参output1，自定义参数类型：string
     args = parser.parse_args(args)
     aa=[{"bucket":"mlflow","path":"MLmodel","destination":""},{"bucket":"mlflow","path":"92/model.pkl","destination":""},{"bucket":"mlflow","path":"model/conda.yaml","destination":""}]
     Path(args.output1).parent.mkdir(parents=True, exist_ok=True)
